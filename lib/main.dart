e97de4e93d6535008a58ac9caefbebe07bcd677b
Akhil\Joshi <akhil.joshi707@gmail.com>
2020-09-20 13:44:51 +0530

    Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.white38,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                  height: double.infinity,
-                  width: 100.0,
-                  color: Colors.red,
-                  child: Text('Hello')),
-              Container(
-                  height: 100.0,
-                  width: 100.0,
-                  color: Colors.yellow,
-                  margin: const EdgeInsets.fromLTRB(20.0 , 100 , 50 , 50 ),
-                  child:  Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.lightGreen,
-                      child: Text('Hello 2'))),
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.blue,
-                  child: Text('Hello 2')),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/img1.jpg'),
+              ),  
+              Text(
+                'Akhil Joshi',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color:Colors.white,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
+              Text(
+                  'FULL STACK DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color:Colors.white,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+              SizedBox(height:20.0,
+                  width:150.0,
+                  child:
+              Divider(
+                color: Colors.teal.shade100,
+              )),
+              Card(
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 25.0),
+                child:Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading:
+                      Icon(Icons.phone , color: Colors.teal,),
+                      title : Text('+91 7123123123',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        )
+                      ),
+
+                  ),
+                )
+              ),
+              Card(
+                  color:Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 25.0),
+                  child:Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListTile(
+                      leading:
+                        Icon(Icons.email , color: Colors.teal,),
+                        title : Text('akhil.joshi@gmail.com',
+                            style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0,
+                            )
+                        ),
+                    ),
+                  )
+              )
             ],
           ),
         ),
